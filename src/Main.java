4a89cf7d662ee9890ccf72f78f826017249e0b19
Vinct-852 <cchunyungg123@gmail.com>
2024-10-31 21:26:48 +0800

    [Add] main structure

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import Game.GameController;
+import Player.Player;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Welcome to the Monopoly Game!");
+            System.out.println("1. Start a new game");
+            System.out.println("2. Resume a game");
+            System.out.println("3. Design game board");
+            System.out.println("4. Quit");
+            System.out.println("Please enter ...");
+            int function = scanner.nextInt();
+
+            switch (function)
+            {
+                case 1:
+                    GameController gameController = new GameController();
+                    break;
+                case 2:
+                    System.out.println("Please input the game data file path");
+                    String filePath = scanner.next();
+                    gameController = new GameController(filePath);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Command not identified.");
+                    break;
+            }
+
+        }
     }
 }
